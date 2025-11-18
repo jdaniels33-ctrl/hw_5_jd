e8c3717e89f31e424a9e28ed6e9ac178f1036939
Josh Daniels <jdaniels33@gmail.com>
2025-11-18 12:33:33 -0700

    lubridated the reported date column from numeric format

--- hw_5.R
+++ hw_5.R
@@ -5,3 +5,9 @@ library(tigris)
 balt_homicides <- homicides %>% 
   filter(city == "Baltimore")
 
+#make a new column for city and state together
+balt_homicides <- balt_homicides %>% 
+  mutate(location = paste(city, state, sep = ", "))
+
+#change reported_date to date format with lubridate
+balt_homicides$reported_date <- ymd(balt_homicides$reported_date)
