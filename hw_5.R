5ca60a30e506d8521b1d8ee7229a8b5668ffb790
Josh Daniels <jdaniels33@gmail.com>
2025-11-20 18:43:09 -0700

    added column for cold months

--- hw_5.R
+++ hw_5.R
@@ -1,8 +1,8 @@
 library(tidyverse)
-library(tigris)
+library(scales)
 
 #load dataset and filter Baltimore homicides
-balt_homicides <- homicides %>% 
+balt_homicides <- read_csv("data/homicide-data.csv") %>% 
   filter(city == "Baltimore")
 
 #make a new column for city and state together
@@ -12,4 +12,12 @@ balt_homicides <- balt_homicides %>%
 #change reported_date to date format with lubridate
 balt_homicides$reported_date <- ymd(balt_homicides$reported_date)
 
-#Need to bin homicides by month
+#Need to indicate which months are Nov - Apr
+balt_homicides <- balt_homicides %>% 
+  mutate(months = month(reported_date))
+
+balt_homicides <- balt_homicides %>% 
+  mutate(cold_vs_warm = if_else(months %in% c(1:4, 11, 12), "cold", "warm"))
+
+         
+
