a9d50bfefa86a8e0e5bc8e8a33b57a9cc91f9053
Josh Daniels <jdaniels33@gmail.com>
2025-11-20 19:03:27 -0700

    added column for year-month

--- hw_5.R
+++ hw_5.R
@@ -19,5 +19,19 @@ balt_homicides <- balt_homicides %>%
 balt_homicides <- balt_homicides %>% 
   mutate(cold_vs_warm = if_else(months %in% c(1:4, 11, 12), "cold", "warm"))
 
+#Need a column that indicates just year and month. Already have a month column
+#in line 15-17, so make a year column and then make another column with both
+#and put into YYYY-MM format.
+
+balt_homicides <- balt_homicides %>% 
+  mutate(yr = year(reported_date))
+
+balt_homicides <- balt_homicides %>% 
+  mutate(yr_month = paste(yr, months, sep = "-"))
+
+balt_homicides$yr_month <- ym(balt_homicides$yr_month)
+#this last column is ugly but should work because year and month are correct
+#Ignore the day!
          
+
 
